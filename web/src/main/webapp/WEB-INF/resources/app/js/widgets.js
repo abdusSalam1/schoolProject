fb1c76c91e18150187931a7d47daf8adaa57dd91
umarbhatti1 <umarbhatti445@gmail.com>
2018-02-05 23:31:02 +0500

    some more Ui settings

--- web/src/main/webapp/WEB-INF/resources/app/js/widgets.js
+++ web/src/main/webapp/WEB-INF/resources/app/js/widgets.js
@@ -78,7 +78,7 @@ angular.module('xe.widgets', []).
 		};
 	}).
 	directive('xeStatusUpdate', function(){
-		
+
 		return {
 			restrict: 'EAC',
 			link: function(scope, el, attrs)
@@ -89,15 +89,15 @@ angular.module('xe.widgets', []).
 					index           = $status_list.filter('.active').index(),
 					auto_switch     = attrDefault($el, 'auto-switch', 0),
 					as_interval		= 0;
-					
+
 				if(auto_switch > 0)
 				{
 					as_interval = setInterval(function()
 					{
 						goTo(1);
-						
+
 					}, auto_switch * 1000);
-					
+
 					$el.hover(function()
 					{
 						window.clearInterval(as_interval);
@@ -107,31 +107,31 @@ angular.module('xe.widgets', []).
 						as_interval = setInterval(function()
 						{
 							goTo(1);
-							
+
 						}, auto_switch * 1000);;
 					});
 				}
-				
+
 				function goTo(plus_one)
 				{
 					index = (index + plus_one) % $status_list.length;
-					
+
 					if(index < 0)
 						index = $status_list.length - 1;
-					
+
 					var $to_hide = $status_list.filter('.active'),
 						$to_show = $status_list.eq(index);
-					
+
 					$to_hide.removeClass('active');
 					$to_show.addClass('active').fadeTo(0,0).fadeTo(320,1);
 				}
-				
+
 				$nav.on('click', function(ev)
 				{
 					ev.preventDefault();
-					
+
 					var plus_one = $(this).hasClass('xe-prev') ? -1 : 1;
-					
+
 					goTo(plus_one);
 				});
 			}
